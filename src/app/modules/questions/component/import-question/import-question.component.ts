e96aef53345d0b2137e510f27b3781a0bd259dd5
Mohit Gondaliya - TatvaSoft <59005579+mohitgondaliya98@users.noreply.github.com>
2023-10-27 10:04:10 +0530

    updated question module

--- src/app/modules/questions/component/import-question/import-question.component.ts
+++ src/app/modules/questions/component/import-question/import-question.component.ts
@@ -7,6 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./import-question.component.scss']
 })
 export class ImportQuestionComponent {
+  optionsList: string[] = ['Option 1', 'Option 2', 'Option 3'];
+
   constructor(private location: Location) { }
 
   handleBackBtn() {
