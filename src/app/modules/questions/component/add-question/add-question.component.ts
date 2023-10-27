99d0720338e9f7f44645dc0b6c3e012b11656dee
Mohit Gondaliya - TatvaSoft <59005579+mohitgondaliya98@users.noreply.github.com>
2023-10-27 22:03:55 +0530

    updated add question page

--- src/app/modules/questions/component/add-question/add-question.component.ts
+++ src/app/modules/questions/component/add-question/add-question.component.ts
@@ -9,9 +9,10 @@ import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 })
 export class AddQuestionComponent {
   optionsList: string[] = ['Option 1', 'Option 2', 'Option 3'];
+  public answerType: string = 'text';
+  public message: string = 'Drag & Drop or <span class="link-primary ml-4p">Browse</span>';
   public Editor = ClassicEditor;
 
-  
   constructor(private location: Location) { }
 
   handleBackBtn() {
