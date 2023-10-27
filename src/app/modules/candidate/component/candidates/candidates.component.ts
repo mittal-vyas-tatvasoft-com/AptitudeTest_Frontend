14f60500dbc82d6a5c8335d0f7b9debd6acaf593
Kinjan <53822503+kinjankoradiya@users.noreply.github.com>
2023-10-27 18:23:38 +0530

    Result Detail page

--- src/app/modules/candidate/component/candidates/candidates.component.ts
+++ src/app/modules/candidate/component/candidates/candidates.component.ts
@@ -10,10 +10,10 @@ import { Router } from '@angular/router';
 export interface CandidateData {
   name: string;
   college: string;
-  yearAdded:string;
   group: string;
   email: string;
   contact: string;
+  yearAdded:string;
   status: string;
   action: string;
 }
@@ -27,22 +27,22 @@ export class CandidatesComponent {
   optionsList: string[] = ['Option 1', 'Option 2', 'Option 3'];
   selectedOption = '10';
   paginationOptionsList: string[] = ['10', '20', '25'];
-  displayedColumns: string[] = ['select', 'name', 'college', 'yearAdded', 'group', 'email', 'contact', 'status', 'action'];
+  displayedColumns: string[] = ['select', 'name', 'college', 'group', 'email', 'contact', 'yearAdded', 'status', 'action'];
   dataSource: MatTableDataSource<CandidateData>;
   selection = new SelectionModel<CandidateData>(true, []);
 
   constructor(public dialog: MatDialog,private router: Router) {
     this.dataSource = new MatTableDataSource([
-      { name: "Ralph Edwards", college: "Columbia University", yearAdded:"2023", group: 'Ahmedabad University', email: 'tanya.hill@example.com', contact: '755 539 6587', status: "Active", action: "" },
-      { name: "Savannah Nguyen", college: "Harvard University", yearAdded:"2023", group: 'Gujarat University', email: 'dolores.chambers@example.com', contact: '755 539 6587', status: "Active", action: "" },
-      { name: "Manufacturing And Packing", college: "Duke University", yearAdded:"2023", group: 'Saurashtra University', email: 'tim.jennings@example.com', contact: '755 539 6587', status: "Inactive", action: "" },
-      { name: "Leslie Alexander", college: "Villanova University", yearAdded:"2023", group: 'Changa University', email: 'felicia.reid@example.com', contact: '755 539 6587', status: "Active", action: "" },
-      { name: "Bessie Cooper", college: "Massachusetts Institute of Technology", yearAdded:"2023", group: 'Ahmedabad University', email: 'curtis.weaver@example.com', contact: '755 539 6587', status: "Active", action: "" },
-      { name: "Manufacturing And Packing", college: "University of North Carolina, Chapel Hill", yearAdded:"2023", group: 'Gujarat University', email: 'jessica.hanson@example.com', contact: '755 539 6587', status: "Active", action: "" },
-      { name: "Kathryn Murphy", college: "Standford University", yearAdded:"2023", group: 'Saurashtra University', email: 'debra.holt@example.com', contact: '755 539 6587', status: "Inactive", action: "" },
-      { name: "Jenny Wilson", college: "St. John's College", yearAdded:"2023", group: 'Changa University', email: 'sara.cruz@example.com', contact: '755 539 6587', status: "Active", action: "" },
-      { name: "Devon Lane", college: "Brigham Young University", yearAdded:"2023", group: 'Ganpat University', email: 'nevaeh.simmons@example.com', contact: '755 539 6587', status: "Inactive", action: "" },
-      { name: "Theresa Webb", college: "Brown University", yearAdded:"2023", group: 'Gujarat University', email: 'michael.mitc@example.com', contact: '755 539 6587', status: "Active", action: "" },
+      { name: "Ralph Edwards", college: "Columbia University", group: 'Ahmedabad University', email: 'tanya.hill@example.com', contact: '755 539 6587', yearAdded:"2023", status: "Active", action: "" },
+      { name: "Savannah Nguyen", college: "Harvard University", group: 'Gujarat University', email: 'dolores.chambers@example.com', contact: '755 539 6587', yearAdded:"2023", status: "Active", action: "" },
+      { name: "Manufacturing And Packing", college: "Duke University", group: 'Saurashtra University', email: 'tim.jennings@example.com', contact: '755 539 6587', yearAdded:"2023", status: "Inactive", action: "" },
+      { name: "Leslie Alexander", college: "Villanova University", group: 'Changa University', email: 'felicia.reid@example.com', contact: '755 539 6587', yearAdded:"2023", status: "Active", action: "" },
+      { name: "Bessie Cooper", college: "Massachusetts Institute of Technology", group: 'Ahmedabad University', email: 'curtis.weaver@example.com', contact: '755 539 6587', yearAdded:"2023", status: "Active", action: "" },
+      { name: "Manufacturing And Packing", college: "University of North Carolina, Chapel Hill", group: 'Gujarat University', email: 'jessica.hanson@example.com', contact: '755 539 6587', yearAdded:"2023", status: "Active", action: "" },
+      { name: "Kathryn Murphy", college: "Standford University", group: 'Saurashtra University', email: 'debra.holt@example.com', contact: '755 539 6587', yearAdded:"2023", status: "Inactive", action: "" },
+      { name: "Jenny Wilson", college: "St. John's College", group: 'Changa University', email: 'sara.cruz@example.com', contact: '755 539 6587', yearAdded:"2023", status: "Active", action: "" },
+      { name: "Devon Lane", college: "Brigham Young University", group: 'Ganpat University', email: 'nevaeh.simmons@example.com', contact: '755 539 6587', yearAdded:"2023", status: "Inactive", action: "" },
+      { name: "Theresa Webb", college: "Brown University", group: 'Gujarat University', email: 'michael.mitc@example.com', contact: '755 539 6587', yearAdded:"2023", status: "Active", action: "" },
     ]);
   }
 
