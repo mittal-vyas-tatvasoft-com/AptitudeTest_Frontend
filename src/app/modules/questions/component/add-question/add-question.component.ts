98588c68a469e097b416f85c5829e1d58b7dd3ca
Mohit Gondaliya - TatvaSoft <59005579+mohitgondaliya98@users.noreply.github.com>
2023-10-30 10:44:46 +0530

    done the html editor

--- src/app/modules/questions/component/add-question/add-question.component.ts
+++ src/app/modules/questions/component/add-question/add-question.component.ts
@@ -9,11 +9,27 @@ import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 })
 export class AddQuestionComponent {
   optionsList: string[] = ['Option 1', 'Option 2', 'Option 3'];
-  public answerType: string = 'text';
+  public answerType: string = 'image';
   public message: string = 'Drag & Drop or <span class="link-primary ml-4p">Browse</span>';
   public Editor = ClassicEditor;
 
   constructor(private location: Location) { }
+
+  // public ckConfig = {
+  //   toolbar: {
+  //     items: [
+  //       'undo', 'redo',
+  //       'heading',
+  //       'color',
+  //       'bold', 'italic', 'underline', 'strikethrough',
+  //       'bulletedList', 'numberedList',
+  //       'outdent', 'indent',
+  //       'imageUpload', 'blockQuote',
+
+  //     ],
+  //     shouldNotGroupWhenFull: true
+  //   },
+  // }
 
   handleBackBtn() {
     this.location.back();
