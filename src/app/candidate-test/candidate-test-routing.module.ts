1e0bb3aa4c51890e1090a579fab1f885946fac89
Meetkumar-Tatva <meet.thakkar@tatvamail.com>
2023-11-28 11:05:48 +0530

    candidate test design

--- src/app/candidate-test/candidate-test-routing.module.ts
+++ src/app/candidate-test/candidate-test-routing.module.ts
@@ -5,26 +5,40 @@ import { LayoutComponent } from './containers/layout/layout.component';
 import { RoleGuard } from '../core/guards/role/role.guard';
 import { Navigation } from '../shared/common/enums';
 import { AuthGuard } from '../core/guards/auth/auth.guard';
-
+import { CandidateInstructionsComponent } from './containers/candidate-instructions/candidate-instructions.component';
+import { TestComponent } from '../modules/test/containers/test/test.component';
+import { CandidateTestComponent } from './containers/candidate-test/components/candidate-test/candidate-test.component';
+import { TestSubmittedComponent } from './containers/test-submitted/test-submitted.component';
 
 const routes: Routes = [
-    {
-        path: '',
-        component: LayoutComponent,
-        canActivate: [AuthGuard, RoleGuard],
-        data: { allowedRoles: [Navigation.RoleUser] },
-        children: [
-            {
-                path: ``,
-                component: DashboardComponent
-            },
-
-        ],
-    },
+  {
+    path: '',
+    component: LayoutComponent,
+    canActivate: [AuthGuard, RoleGuard],
+    data: { allowedRoles: [Navigation.RoleUser] },
+    children: [
+      {
+        path: ``,
+        component: DashboardComponent,
+      },
+      {
+        path: `instructions`,
+        component: CandidateInstructionsComponent,
+      },
+      {
+        path: `test`,
+        component: CandidateTestComponent,
+      },
+      {
+        path: `submitted`,
+        component: TestSubmittedComponent,
+      },
+    ],
+  },
 ];
 
 @NgModule({
-    imports: [RouterModule.forChild(routes)],
-    exports: [RouterModule]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
 })
-export class CandidateTestRoutingModule { }
+export class CandidateTestRoutingModule {}
