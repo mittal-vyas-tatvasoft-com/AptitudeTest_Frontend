293e26ba1672b9a6a67b518c1c2da3a58256b980
Meetkumar-Tatva <meet.thakkar@tatvamail.com>
2023-11-30 17:34:59 +0530

    Display Question in Live Candidate Test

--- src/app/candidate-test/containers/candidate-test/components/mcq-test/mcq-test.component.ts
+++ src/app/candidate-test/containers/candidate-test/components/mcq-test/mcq-test.component.ts
@@ -3,7 +3,9 @@ import { Router } from '@angular/router';
 import { Question } from 'src/app/candidate-test/interfaces/candidate-test.interface';
 import { CandidateTestService } from 'src/app/candidate-test/services/candidate-test.service';
 import { LoginService } from 'src/app/core/auth/services/login.service';
-import { QuestionStatus } from 'src/app/shared/common/enums';
+import { QuestionStatus, StatusCode } from 'src/app/shared/common/enums';
+import { ResponseModel } from 'src/app/shared/common/interfaces/response.interface';
+import { SnackbarService } from 'src/app/shared/snackbar/snackbar.service';
 
 @Component({
   selector: 'app-mcq-test',
@@ -13,28 +15,41 @@ import { QuestionStatus } from 'src/app/shared/common/enums';
 export class McqTestComponent implements OnInit {
   firstName: string;
   lastName: string;
+  userId: number = 0;
+  testId: number = 0;
   timeRemaining = {
     hours: 1,
     minutes: 20,
     seconds: 0,
   };
   seconds = 0;
-  totalCount = 50;
-  currentCount = 35;
   question: Question = {
-    questionText:
-      'Occaecat in sint nulla cillum exercitation culpa aliquip enimconsequat quis adipisicing do dolor qui reprehenderit excepteur.',
-    options: ['Option A', 'Option B', 'Option C', 'Option D'],
-    optionType: 1,
-    questionType: 1,
+    id: 0,
+    questionText: '',
+    options: [],
+    optionType: 0,
+    questionType: 0,
+    difficulty: 0,
+    nextQuestionId: -1,
+    questionNumber: 0,
+    totalQuestions: 0,
   };
 
   constructor(
     public loginService: LoginService,
     private router: Router,
-    private testService: CandidateTestService
+    private testService: CandidateTestService,
+    private snackBarService: SnackbarService
   ) {}
+
   ngOnInit(): void {
+    const candidateDetails = this.loginService.decodeToken();
+    this.firstName = candidateDetails.FirstName;
+    this.lastName = candidateDetails.Name;
+    this.userId = candidateDetails.Id;
+
+    this.displayQuestion();
+
     this.seconds =
       this.timeRemaining.hours * 3600 +
       this.timeRemaining.minutes * 60 +
@@ -50,22 +65,34 @@ export class McqTestComponent implements OnInit {
         seconds: seconds,
       };
     }, 1000);
-    const candidateDetails = this.loginService.decodeToken();
-    this.firstName = candidateDetails.FirstName;
-    this.lastName = candidateDetails.Name;
+  }
+
+  displayQuestion() {
+    if (this.question.nextQuestionId && this.userId && this.testId) {
+      this.testService
+        .getQuestion(this.question.nextQuestionId, this.userId, this.testId)
+        .subscribe({
+          next: (response: ResponseModel<Question>) => {
+            if (response.statusCode == StatusCode.Success) {
+              this.question = response.data;
+            } else {
+              this.snackBarService.error(response.message);
+            }
+          },
+        });
+    }
   }
 
   onSubmit(event: boolean[]) {
-    if (this.currentCount == this.totalCount) {
+    this.displayQuestion();
+    if (this.question.questionNumber == this.question.totalQuestions) {
       this.router.navigate(['/user/submitted']);
     }
-    this.currentCount++;
-    this.testService;
     let state =
       event.filter(Boolean).length > 0
         ? QuestionStatus.Answered
         : QuestionStatus.Skipped;
-    let status = [this.currentCount - 1, state];
+    let status = [this.question.questionNumber, state];
     this.testService.questionStatus.next(status);
   }
 }
--- src/app/candidate-test/interfaces/candidate-test.interface.ts
+++ src/app/candidate-test/interfaces/candidate-test.interface.ts
@@ -1,6 +1,11 @@
 export interface Question {
+  id: number;
+  difficulty: number;
   questionText: string;
   questionType: number;
   optionType: number;
+  nextQuestionId: number;
+  questionNumber: number;
   options: string[];
+  totalQuestions: number;
 }
