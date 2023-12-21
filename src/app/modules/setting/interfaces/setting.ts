dcaa7bc8f179b5944d481047a941947984478e8a
harshtatva2002 <harsh.pateltraining@gmail.com>
2023-12-21 14:42:42 +0530

    changes for new setting added

--- src/app/modules/setting/interfaces/setting.ts
+++ src/app/modules/setting/interfaces/setting.ts
@@ -1,6 +1,7 @@
 export interface Setting {
   userRegistration: boolean;
   camera: boolean;
+  clearResponseButton: boolean;
   screenCapture: boolean;
   intervalForScreenCapture: number;
   cutOff: number;
