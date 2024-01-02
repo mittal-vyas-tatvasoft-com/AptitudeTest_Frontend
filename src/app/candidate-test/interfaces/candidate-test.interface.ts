962c144544b8c9e1369b435f4f7c59cdeb6d68b7
RajviKotecha <rajvi.kotecha@tatvasoft.com>
2024-01-02 18:48:39 +0530

    minor changes

--- src/app/candidate-test/interfaces/candidate-test.interface.ts
+++ src/app/candidate-test/interfaces/candidate-test.interface.ts
@@ -3,6 +3,7 @@ export interface Question {
   difficulty: number;
   questionText: string;
   questionType: number;
+  topic: number;
   optionType: number;
   nextQuestionId: number;
   questionNumber: number;
