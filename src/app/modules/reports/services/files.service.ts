b33f5d3235f036533ead1b41f5e7746af0bdf469
Meetkumar-Tatva <meet.thakkar@tatvamail.com>
2024-01-03 10:18:39 +0530

    Revert "Removed exttra console log"
    This reverts commit 268a6fc0331704cd6a13af0ea31d3f1de9922aac.

--- src/app/modules/reports/services/files.service.ts
+++ src/app/modules/reports/services/files.service.ts
@@ -38,6 +38,7 @@ export class FilesService {
   }
 
   updateIds(fileElement: FileElement) {
+    console.log('Updating', this.level, fileElement);
     switch (this.level) {
       case ReportLevels.Test:
         this.test = fileElement.data;
