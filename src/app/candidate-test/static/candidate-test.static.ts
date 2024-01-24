f684d46d22ee25d9696c01207fc45bc60f65c20d
Meetkumar-Tatva <meet.thakkar@tatvamail.com>
2024-01-24 14:18:13 +0530

    Screen capture

--- src/app/candidate-test/static/candidate-test.static.ts
+++ src/app/candidate-test/static/candidate-test.static.ts
@@ -1,2 +1,6 @@
 export const NoTestAssigned =
   "We're sorry, but we couldn't find any information related to your exam!!";
+
+export const Messages = {
+  ScreenCaptureError: 'Please Share Entire Screen',
+};
