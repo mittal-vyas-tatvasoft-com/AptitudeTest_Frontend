f91d0f9c6b624f01a2feee30566c5fd93e565c98
mittal.vyas@tatvasoft.com <mittal.vyas@tatvasoft.com>
2024-03-11 16:55:22 +0530

    Wrong result bug fixed

--- src/app/shared/dialogs/confirmation-dialog/confirmation-dialog.component.ts
+++ src/app/shared/dialogs/confirmation-dialog/confirmation-dialog.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { ConfirmDialogData } from '../../common/interfaces/dialogdata.interface';
+import { CandidateTestService } from 'src/app/candidate-test/services/candidate-test.service';
 
 @Component({
   selector: 'app-confirmation-dialog',
@@ -10,10 +11,12 @@ import { ConfirmDialogData } from '../../common/interfaces/dialogdata.interface'
 export class ConfirmationDialogComponent {
   constructor(
     private dialogRef: MatDialogRef<ConfirmationDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: ConfirmDialogData
+    @Inject(MAT_DIALOG_DATA) public data: ConfirmDialogData,
+    public candidateTestService: CandidateTestService
   ) {}
   confirmed(result: boolean) {
     if (result) {
+      this.candidateTestService.isEndingTest = true;
       this.dialogRef.close(result);
     } else {
       this.dialogRef.close(false);
