c9a20054fde0fa13143313a799296693e3e49211
RajviKotecha <rajvi.kotecha@tatvasoft.com>
2024-04-10 10:09:35 +0530

    change

--- src/app/modules/setting/setting/setting.component.ts
+++ src/app/modules/setting/setting/setting.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Subscription } from 'rxjs';
 import { ResponseModel } from 'src/app/shared/common/interfaces/response.interface';
+import { validations } from 'src/app/shared/messages/validation.static';
 import { SnackbarService } from 'src/app/shared/snackbar/snackbar.service';
 import { SettingControls } from '../configs/setting.config';
 import { Setting } from '../interfaces/setting';
@@ -30,6 +31,14 @@ export class SettingComponent implements OnInit {
       screenCapture: [''],
       intervalForScreenCapture: [0, [Validators.min(0)]],
       cutOff: [0, [Validators.min(0)]],
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(validations.common.emailREGEX),
+        ],
+      ],
+      password: ['', Validators.required],
     });
 
     this.settingService.get().subscribe({
@@ -41,6 +50,8 @@ export class SettingComponent implements OnInit {
           screenCapture: res.data.screenCapture,
           intervalForScreenCapture: +res.data.intervalForScreenCapture,
           cutOff: +res.data.cutOff,
+          email: res.data.email,
+          password: res.data.password,
         });
       },
       error: (error) => {
@@ -61,5 +72,17 @@ export class SettingComponent implements OnInit {
           this.snackbarService.error(error.message);
         },
       });
+  }
+
+  onIconClick(event: any) {
+    if (event.inputType === 'text') {
+      event.inputType = 'password';
+      this.settingControls.Password.iconName =
+        'password-visibility-show-dark.svg';
+    } else {
+      event.inputType = 'text';
+      this.settingControls.Password.iconName =
+        'password-visibility-hide-dark.svg';
+    }
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  // baseURL: 'https://localhost:7282/api/',
-  // imageBase: 'https://localhost:7282',
+  //baseURL: 'https://localhost:7282/api/',
+  //imageBase: 'https://localhost:7282',
   imageBase: 'https://aptitudetest-backend-uat.web2.anasource.com',
   baseURL: 'https://aptitudetest-backend-uat.web2.anasource.com/api/',
 };
